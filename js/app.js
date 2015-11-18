e737f21cbcf7385279a1d46ebb4495085f3e88c7
tbindrup <tbindrup1221@gmail.com>
2015-11-18 09:32:47 -0500

    contact form workings

--- js/app.js
+++ js/app.js
@@ -232,7 +232,7 @@ String.prototype.toProperCase = function () {
       $modalInstance.close();
     };
   };
-  function ContactController($scope, $state, $stateParams) {
+  function ContactController($scope, $state, $stateParams, Emailer) {
     var cc = this;
     cc.formValid = false;
     cc.emailSent = false;
@@ -290,6 +290,20 @@ String.prototype.toProperCase = function () {
   };
 
   // SERVICES
+  function Emailer($http) {
+    return {
+      SendContactEmail: function(the_data) {
+        return $http({
+          method: 'POST',
+          url: 'utils/send_email.php',
+          headers: {
+            'Content-Type':'application/json'
+          },
+          data: the_data
+        });
+      }
+    }
+  };
   function TechnologyDetails($http, $sce, $sessionStorage, _) {
     var techData = $sessionStorage.techData || {
       'technologies': null,
@@ -378,94 +392,6 @@ String.prototype.toProperCase = function () {
     return {
       'dimensions': dimensions
     };
-  };
-  function DataTransfer($http) {
-    return {
-      SendContactEmail: function(the_data) {
-        return $http({
-          method: 'POST',
-          url: 'https://mandrillapp.com/api/1.0/messages/send.json',
-          headers: {
-            'Content-Type':'application/json'
-          },
-          data: {
-            "key":"h_FdIHNlZN0YdLY8vU8Cfg",
-            "message": {
-              "text": 'Name: '+the_data.name+'\nEmail Address: '+the_data.email+'\nPhone Number: '+the_data.phone+'\nMessage: '+the_data.message,
-              "subject": "You have a new message for the RBA site.",
-              "from_email": "signupforroyalbusinessacademy@gmail.com",
-              "from_name": "New Message from RBA site",
-              "to": [
-                {
-                  "email": "spencer@royalbusinessacademy.org ",
-                  "name": "Spencer Rogers",
-                  "type": "to"
-                }
-              ]
-            }
-          }
-        });
-      },
-      SendApplicationEmail: function() {
-        return $http({
-          method: 'POST',
-          url: 'https://mandrillapp.com/api/1.0/messages/send.json',
-          headers: {
-            'Content-Type':'application/json'
-          },
-          data: {
-            "key":"h_FdIHNlZN0YdLY8vU8Cfg",
-            "message": {
-              "text": 'You just had a new student apply for Royal Business Academy!',
-              "subject": "New RBA Applicant",
-              "from_email": "signupforroyalbusinessacademy@gmail.com",
-              "from_name": "New Message from RBA site",
-              "to": [
-                {
-                  "email": "spencer@royalbusinessacademy.org ",
-                  "name": "Spencer Rogers",
-                  "type": "to"
-                }
-              ]
-            }
-          }
-        });
-      },
-      SendApplication: function(the_data) {
-        return $http({
-          method: 'POST',
-          url: 'https://docs.google.com/forms/d/1hn7YvTiMZZhA3FEm7-UHagXZDvifUx5VbLdRgz37_nE/formResponse',
-          headers: {
-            'Content-Type':'application/x-www-form-urlencoded'
-          },
-          transformRequest: function(obj) {
-            var str = [];
-            for(var p in obj)
-            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-            return str.join("&");
-          },
-          data: {
-            'entry.1837275675': the_data.first_name || '',
-            'entry.2057115759': the_data.preferred_name || '',
-            'entry.1691304936': the_data.native_language || '',
-            'entry.961943981': the_data.other_languages || '',
-            'entry.765568650': the_data.family_surname || '',
-            'entry.1496721659': the_data.date_of_birth || '',
-            'entry.1058894701': the_data.how_did_you_hear_about_us || '',
-            'entry.1267007426': the_data.gender || '',
-            'entry.1248163241': the_data.street_address || '',
-            'entry.1538648643': the_data.state_province || '',
-            'entry.326672777': the_data.postal_code || '',
-            'entry.757327993': the_data.telephone_number || '',
-            'entry.716204733': the_data.city || '',
-            'entry.753284705': the_data.country || '',
-            'entry.344209816': the_data.email || '',
-            'entry.629576774': the_data.application_year || '',
-            'entry.809888668': the_data.level_of_education || ''
-          }
-        });
-      }
-    }
   };
 
   // RANDOM GLOBAL UTILITIES
